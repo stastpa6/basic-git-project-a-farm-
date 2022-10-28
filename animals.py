464c367908700c3e651f6c856c79bd12c4ae6a98
Pavel Stastny <pavel.stastny@fs.cvut.cz>
2022-10-28 23:37:16 +0200

    defined all animals as instances of Animal and gave them properties

--- animals.py
+++ animals.py
@@ -9,3 +9,9 @@ class Animal:
     def __init__(self, name: str, sound: str):
         self.name = name
         self.sound = sound
+
+Cow = Animal(Cow, moo)
+Chicken = Animal(Chicken, cha-caw)
+Goat = Animal(Goat, maa)
+Dog = Animal(Dog, woof)
+Sheep = Animal(Sheep, bee)
